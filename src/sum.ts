2a5c9f8024f1e9603319251e0b7aebc24ffdf3ee
Jonathan Eskew <jonathan@jeskew.net>
2018-03-12 01:00:00 -0700

    perf: ensure sum uses synchronous iteration on sync iterators

--- src/sum.ts
+++ src/sum.ts
@@ -1,12 +1,36 @@
+import { isAsyncIterable } from './isIterable';
+
 /**
  * @returns the sum of all values yielded by the provided `iterable`.
  */
 export async function sum(
     iterable: Iterable<number>|AsyncIterable<number>
 ): Promise<number> {
+    if (isAsyncIterable(iterable)) {
+        return sumAsync(iterable);
+    }
+
+    return sumSync(iterable);
+}
+
+export function sumSync(iterable: Iterable<number>): number {
     let sum = 0;
-    for await (const element of iterable) {
+    for (const element of iterable) {
         sum += element;
+    }
+
+    return sum;
+}
+
+async function sumAsync(iterable: AsyncIterable<number>): Promise<number> {
+    let sum = 0
+    for (
+        let iterator = iterable[Symbol.asyncIterator](),
+            next = await iterator.next();
+        !next.done;
+        next = await iterator.next()
+    ) {
+        sum += next.value;
     }
 
     return sum;
