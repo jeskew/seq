5a0378aadcdfe087fb79b07633a20f29bafbc52f
Jonathan Eskew <jonathan@jeskew.net>
2018-03-11 17:10:04 -0700

    perf: ensure find uses synchronous iteration on sync iterators

--- src/find.spec.ts
+++ src/find.spec.ts
@@ -1,21 +1,43 @@
 import { find, range } from '.';
-import { asyncFibonacci } from './testIterators.fixture';
+import {
+    AsyncFibonacciSequence,
+    asyncify,
+    CloseHandlingIterator,
+    fibonacci,
+} from './testIterators.fixture';
 import * as test from 'tape';
 
 test('find', async t => {
-    t.plan(2)
+    t.plan(5)
 
     t.equal(
         144,
-        await find((num: number) => num % 12 === 0, asyncFibonacci()),
+        await find((num: number) => num % 12 === 0, fibonacci()),
         'finds elements in infinite sequences'
+    )
+
+    t.equal(
+        144,
+        await find((num: number) => num % 12 === 0, new AsyncFibonacciSequence()),
+        'finds elements in infinite async sequences'
+    )
+
+    const iter = new CloseHandlingIterator
+    await find(() => true, iter);
+    t.ok(
+        iter.returnCalled,
+        'calls .return on the underlying iterator when a match is found'
     )
 
     try {
         await find((num: number) => num > 100, range(5))
-        t.fail('should have thrown when no sequence member satisfying the predicate could be found')
     } catch {
         t.pass('throws when no sequence members satisfy the predicate')
     }
 
+    try {
+        await find((num: number) => num > 100, asyncify(range(5)))
+    } catch {
+        t.pass('throws when no async sequence members satisfy the predicate')
+    }
 })
--- src/find.ts
+++ src/find.ts
@@ -1,3 +1,5 @@
+import { isSyncIterable } from './isIterable';
+
 /**
  * Locates the first value yielded by the supplied iterable that satisfies the
  * supplied predicate.
@@ -7,12 +9,48 @@
  *                  a boolean.
  */
 export async function find<T>(
-    predicate: (arg: T) => boolean|Promise<boolean>,
+    predicate: (arg: T) => boolean,
     iterable: Iterable<T>|AsyncIterable<T>
 ) {
-    for await (const element of iterable) {
-        if (await predicate(element)) {
+    if (isSyncIterable(iterable)) {
+        return findSync(predicate, iterable);
+    }
+
+    return findAsync(predicate, iterable);
+}
+
+export function findSync<T>(
+    predicate: (arg: T) => boolean,
+    iterable: Iterable<T>
+): T {
+    for (const element of iterable) {
+        if (predicate(element)) {
             return element;
+        }
+    }
+
+    throw new Error('No yielded value satisfied the provided condition');
+}
+
+async function findAsync<T>(
+    predicate: (arg: T) => boolean,
+    iterable: AsyncIterable<T>
+): Promise<T> {
+    const iterator = iterable[Symbol.asyncIterator]();
+    for (
+        let next = await iterator.next(),
+            value = next.value,
+            done = next.done;
+        !done;
+        next = await iterator.next(),
+        value = next.value,
+        done = next.done
+    ) {
+        if (predicate(value)) {
+            if (typeof iterator.return === 'function') {
+                await iterator.return();
+            }
+            return value;
         }
     }
 
