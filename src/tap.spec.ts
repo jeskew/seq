b9b7a67434ed08b8632dedce46afea8eebcb2f4b
Jonathan Eskew <jonathan@jeskew.net>
2018-03-12 23:50:36 -0700

    perf: use await instead of tslib in tap

--- src/tap.spec.ts
+++ src/tap.spec.ts
@@ -1,5 +1,6 @@
 import { range, tap } from '.';
 import * as test from 'tape';
+import { DECORATOR_ERROR_TEST_COUNT, testDecoratorErrorHandling } from './testIterators.fixture';
 
 test('tap', async t => {
     t.plan(1)
@@ -9,4 +10,10 @@ test('tap', async t => {
     for await (const _ of tapTransducer(range(5))) {}
 
     t.equal(count, 5)
-})
+});
+
+test('tap error handling', async t => {
+    t.plan(DECORATOR_ERROR_TEST_COUNT);
+
+    testDecoratorErrorHandling(tap.bind(null, async () => {}), t, 'tap');
+});
--- src/tap.ts
+++ src/tap.ts
@@ -1,3 +1,5 @@
+import { iteratorFromIterable } from './iteratorFromIterable';
+
 /**
  * Execute an action for each value yielded by the provided iterable.
  *
@@ -8,12 +10,41 @@
  * @returns a iterable that will yield all values yielded by the provided
  * `iterable`.
  */
-export async function *tap<T>(
+export function tap<T>(
     action: (arg: T) => void|Promise<void>,
     iterable: Iterable<T>|AsyncIterable<T>
-) {
-    for await (const element of iterable) {
-        await action(element);
-        yield element;
+): AsyncIterableIterator<T> {
+    return new TapIterator(action, iterable);
+}
+
+class TapIterator<T> implements AsyncIterableIterator<T> {
+    private readonly iterator: Iterator<T>|AsyncIterator<T>;
+
+    constructor(
+        private readonly action: (arg: T) => void|Promise<void>,
+        iterable: Iterable<T>|AsyncIterable<T>
+    ) {
+        this.iterator = iteratorFromIterable(iterable);
+    }
+
+    [Symbol.asyncIterator]() {
+        return this;
+    }
+
+    async next() {
+        const { done, value } = await this.iterator.next();
+        if (!done) {
+            await this.action(value);
+        }
+
+        return { done, value };
+    }
+
+    async return() {
+        if (typeof this.iterator.return === 'function') {
+            return this.iterator.return();
+        }
+
+        return { done: true } as IteratorResult<T>;
     }
 }
