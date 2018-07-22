4da08a0c696b265e24de05d9d3a20c66de75bd91
Jonathan Eskew <jonathan@jeskew.net>
2018-07-22 11:30:14 -0700

    Export helper methods

--- src/isIterable.ts
+++ src/isIterable.ts
@@ -1,7 +1,17 @@
+/**
+ * Determines if the provided value adheres to the AsyncIterable protocol.
+ *
+ * @param arg Any value
+ */
 export function isAsyncIterable<T>(arg: any): arg is AsyncIterable<T> {
     return Boolean(arg) && typeof arg[Symbol.asyncIterator] === 'function';
 }
 
+/**
+ * Determines if the provided value adheres to Iterable protocol.
+ *
+ * @param arg Any value
+ */
 export function isSyncIterable<T>(arg: any): arg is Iterable<T> {
     return Boolean(arg) && typeof arg[Symbol.iterator] === 'function';
 }
