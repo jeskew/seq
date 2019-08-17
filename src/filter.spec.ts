2d785418f58335c10f28231c725a218fbfddee7e
Jonathan Eskew <jonathan@jsq.io>
2019-08-17 11:17:27 -0400

    chore: Update all the things

--- src/filter.spec.ts
+++ src/filter.spec.ts
@@ -2,19 +2,19 @@ import { collect, filter, take } from '.';
 import { asyncFibonacci, fibonacci, DECORATOR_ERROR_TEST_COUNT, testDecoratorErrorHandling } from './testIterators.fixture';
 import * as test from 'tape';
 
-const filterEvens = filter.bind(null, (num: number) => num % 2 === 0)
+const acceptEvens = (num: number) => num % 2 === 0;
 
 test('filter retains all elements that satisfy the predicate and discards the rest', async t => {
     t.plan(2)
 
     t.deepEqual(
         [2, 8, 34],
-        [...filterEvens(take(10, fibonacci()))]
+        [...filter(acceptEvens, take(10, fibonacci()))]
     )
 
     t.deepEqual(
         [2, 8, 34],
-        await collect(filterEvens(take(10, asyncFibonacci())))
+        await collect(filter(acceptEvens, take(10, asyncFibonacci())))
     )
 })
 
@@ -23,12 +23,12 @@ test('filter is applied lazily', async t => {
 
     t.deepEqual(
         [2, 8, 34, 144, 610],
-        [...take(5, filterEvens(fibonacci()) as IterableIterator<number>)]
+        [...take(5, filter(acceptEvens, fibonacci()) as IterableIterator<number>)]
     )
 
     t.deepEqual(
         [2, 8, 34, 144, 610],
-        await collect(take(5, filterEvens(asyncFibonacci())))
+        await collect(take(5, filter(acceptEvens, asyncFibonacci())))
     )
 })
 
